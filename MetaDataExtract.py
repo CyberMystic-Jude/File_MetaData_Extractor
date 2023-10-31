c628538d3b7ab578e6a8504256ba3cb3d14f063a
Jude <146963010+CyberMystic-Jude@users.noreply.github.com>
2023-10-31 23:53:46 +0530

    Update MetaDataExtract.py

--- MetaDataExtract.py
+++ MetaDataExtract.py
@@ -9,20 +9,17 @@ import magic
 
 def extract_file_metadata(file_path):
     try:
-        # Get file metadata using os.stat
+       
         file_stat = os.stat(file_path)
         
-        # Extract relevant file metadata
         creation_time = file_stat.st_ctime
         modification_time = file_stat.st_mtime
         access_time = file_stat.st_atime
         size = file_stat.st_size
 
-        # Get file type
         mime = magic.Magic()
         file_type = mime.from_file(file_path)
 
-        # Initialize other metadata variables
         author = None
         owner = file_stat.st_uid
         permissions = oct(stat.S_IMODE(file_stat.st_mode))
@@ -32,36 +29,28 @@ def extract_file_metadata(file_path):
         audio_bitrate = None
         video_resolution = None
 
-        # Convert timestamps to human-readable format
         creation_time_str = time.ctime(creation_time)
         modification_time_str = time.ctime(modification_time)
         access_time_str = time.ctime(access_time)
 
-        # For images (JPEG, PNG, etc.)
         if file_type.startswith("image"):
             with Image.open(file_path) as img:
                 exif_data = img._getexif()
                 if exif_data:
-                    author = exif_data.get(315, None)  # 315 is the EXIF tag for Author/Artist
-                    gps_coordinates = exif_data.get(34853, None)  # 34853 is the EXIF tag for GPS Info
+                    author = exif_data.get(315, None)  
+                    gps_coordinates = exif_data.get(34853, None) 
 
-        # For PDF files
         if file_type == "application/pdf":
             with open(file_path, "rb") as pdf_file:
                 pdf_reader = PdfFileReader(pdf_file)
                 author = pdf_reader.getDocumentInfo().author
 
-        # For audio files (MP3, etc.)
         if file_type.startswith("audio"):
-            # You may use a specialized library to extract audio metadata
             pass
 
-        # For video files
         if file_type.startswith("video"):
-            # You may use a specialized library to extract video metadata
             pass
 
-        # Print extracted metadata
         print("---File Meta Data Extractor ~by JUDE---")
         print()
         print(f"File: {file_path}")
